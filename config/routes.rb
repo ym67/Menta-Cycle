fc39cdfd80e5a1f4b1a1a5019d963f8132d6a91a
Yusuke Matsuda <y.santamaria567@gmail.com>
2019-10-14 15:46:06 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'big5_scores#index'
+
+  resources :big5_scores
 end
---
+++ spec/factories/big5_scores.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :big5_score do
+    neuroticism { 1 }
+    extraversion { 1 }
+    openness_to_experience { 1 }
+    agreeableness { 1 }
+    conscientiousness { 1 }
+  end
+end
