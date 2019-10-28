563137cd5ac372bbe3e8eeda678d1d4c1ba1bd35
Yusuke Matsuda <y.santamaria567@gmail.com>
2019-10-28 19:12:24 +0900

    sss(詳細ストレス計測機能)を実装

---
+++ app/controllers/ssses_controller.rb
@@ -0,0 +1,59 @@
+class SssesController < ApplicationController
+  before_action :set_sss, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @ssses = Sss.all
+  end
+
+  def show
+  end
+
+  def new
+    @sss = Sss.new
+  end
+
+  def edit
+  end
+
+  def create
+    @sss = current_user.ssses.build(sss_params)
+      if @sss.save
+        #１日につきストレス値は最新の１件のみを保存するようにする（それ以外は削除）
+        while Sss.where(user_id: current_user.id).where(created_at: @sss.created_at.in_time_zone.all_day).length >= 2 do
+          Sss.where(user_id: current_user.id).where(created_at: @sss.created_at.in_time_zone.all_day).first.destroy
+        end
+        if Pss4.where(user_id: current_user.id).where(created_at: @sss.created_at.in_time_zone.all_day).length > 0
+          Pss4.where(user_id: current_user.id).where(created_at: @sss.created_at.in_time_zone.all_day).last.destroy
+        end
+        redirect_to @sss, notice: '診断終了'
+      else
+        render :new
+      end
+  end
+
+  def update
+    if @sss.update(sss_params)
+      redirect_to @sss, notice: '更新'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @sss.destroy
+    redirect_to ssses_url, notice: '削除'
+  end
+
+  private
+  def sss_params
+    params.require(:sss).permit(:q1, :q2, :q3, :q4, :q5, :q6, :q7, :q8, :q9, :q10,
+                                :q11, :q12, :q13, :q14, :q15, :q16, :q17, :q18, :q19, :q20,
+                                :q21, :q22, :q23, :q24, :q25, :q26, :q27, :q28, :q29, :q30,
+                                :q31, :q32, :q33, :q34, :q35, :q36, :q37, :q38, :q39, :q40,
+                                :score, :user_id)
+  end
+
+  def set_sss
+    @sss = Sss.find(params[:id])
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,19 @@ class UsersController < ApplicationController
   before_action :authenticate_user! #, only: [:new, :edit, :update, :destroy]
 
   def show
-    @dailry_data = StressDiary.where(user_id: current_user).order(id: :desc) + Pss4.where(user_id: current_user)
-    @pss4_scores = Pss4.where(user_id: current_user).limit(31).pluck(:score, :created_at)
+    @dailry_data = StressDiary.where(user_id: current_user).order(id: :desc) + Pss4.where(user_id: current_user) + Sss.where(user_id: current_user)
+    @stress_scores = (Pss4.where(user_id: current_user).order(:created_at).limit(50) + Sss.where(user_id: current_user).order(:created_at).limit(50)).pluck(:score, :created_at, :q40).sort_by { |_, b| b }
   end
 
   def dailry_data
     @date = params[:para]
     @dailry_stress_diaries = StressDiary.where(created_at: params[:para].in_time_zone.all_day).where(user_id: current_user).order(id: :desc)
-    @dailry_pss4_score = Pss4.where(created_at: params[:para].in_time_zone.all_day).where(user_id: current_user).last
+    if Pss4.where(created_at: params[:para].in_time_zone.all_day).where(user_id: current_user).last == nil && Sss.where(created_at: params[:para].in_time_zone.all_day).where(user_id: current_user).last != nil
+      # @dailry_stress_score = ((Sss.where(created_at: params[:para].in_time_zone.all_day).where(user_id: current_user).last.score).to_f / 10)
+      @dailry_stress_score = Sss.where(created_at: params[:para].in_time_zone.all_day).where(user_id: current_user).last
+    else
+      @dailry_stress_score = Pss4.where(created_at: params[:para].in_time_zone.all_day).where(user_id: current_user).last.score
+    end
   end
 
   private
